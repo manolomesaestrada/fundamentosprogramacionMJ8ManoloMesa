a0173cb9b45aa7648856cf7a8e07ebb6149300f8
manolomesaestrada <manolo.mesa@upb.edu.co>
2026-07-30 08:15:10 -0500

    primer commit

---
+++ variablesconstantes/variablesconstantes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace variablesconstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //aqui va el codigo fuente
+            //variables
+            string Name = "tilin el creador";
+            Console.WriteLine("w" + Name+" curso de fundamentos de programacion");
+
+
+        }
+    }
+}
