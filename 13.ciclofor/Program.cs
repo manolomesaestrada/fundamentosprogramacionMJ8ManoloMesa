b56321b7e5ad0b5d82adbbc6afbd23a4de60fc95
manolomesaestrada <manolo.mesa@upb.edu.co>
2026-09-03 09:27:59 -0500

    Add files via upload

---
+++ 13.ciclofor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace _13.ciclofor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int contador = 1; contador <= 5; contador++) {
+                acumulador += contador;
+            }
+
+            Console.WriteLine($"La suma de los primeros 5 números es: {acumulador}");
+        }
+    }
+}
