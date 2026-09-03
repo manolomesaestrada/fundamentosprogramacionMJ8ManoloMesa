431808cd1e2e14a599962cb3b84184c26d2fd740
manolomesaestrada <manolo.mesa@upb.edu.co>
2026-09-03 11:32:37 -0500

    Add files via upload

---
+++ 13,2.ciclofor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_2.ciclofor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un programa que permita imprimir por pantalla los números múltiplos de 5, el usuario debe ingresar por el teclado el rango de números a evaluar. 
+            int acumulator = 0;
+            int count = 0;
+            for (int multiplo = 0; multiplo < 10; multiplo++)
+            {
+                if (multiplo % 5 == 0)
+                {
+                    Console.WriteLine(multiplo);
+                    acumulator += multiplo;
+                    count++;
+                }
+            }
+            Console.WriteLine("La suma de los múltiplos de 5 es: " + acumulator);
+            Console.WriteLine("El número de múltiplos de 5 es: " + count);
+        }
+    }
+}
